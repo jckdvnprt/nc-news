14a9c5ec1d1329c0d63c8dad0cd4182120ff30fa
Jack Davenport <jckwdvnprt@gmail.com>
2024-05-03 14:47:34 +0100

    Init

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+     
+    
+      </div>
+      <h1>Northcoders News</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+    
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
