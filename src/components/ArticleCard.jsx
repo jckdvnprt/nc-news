01a3c70ddbf690ee11f663e9f26f92d9e522dede
Jack Davenport <jckwdvnprt@gmail.com>
2024-05-07 16:37:59 +0100

    Refactoring voting system

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { fetchArticleById } from '../utils/articleApi'; 
+import { fetchArticleById } from '../utils/articleApi';
 import { fetchCommentsById } from '../utils/commentsApi';
-import CommentCard from './CommentsCard'; 
+import CommentCard from './CommentsCard';
 import { getArticleVotes } from '../utils/getArticleVotesApi';
 import updateVotes from '../utils/updateVotesApi';
 
@@ -11,9 +11,9 @@ function IndividualArticleCard() {
   const [article, setArticle] = useState(null);
   const [loading, setLoading] = useState(true);
   const [articleComments, setArticleComments] = useState([]);
-  const [showComments, setShowComments] = useState(false); 
+  const [showComments, setShowComments] = useState(false);
   const [currentVotes, setCurrentVotes] = useState(0);
-  const [userVote, setUserVote] = useState(null); 
+  const [userVote, setUserVote] = useState(null);
 
   useEffect(() => {
     Promise.all([fetchArticleById(id), fetchCommentsById(id), getArticleVotes(id)])
@@ -23,33 +23,24 @@ function IndividualArticleCard() {
         setCurrentVotes(votes.votes);
         setLoading(false);
       })
+      .catch((error) => console.error(error));
   }, [id]);
-  
-  const toggleComments = () => {
-    setShowComments(!showComments); 
-  };
 
+  const toggleComments = () => {
+    setShowComments(!showComments);
+  };
   const handleVote = async (voteType) => {
     try {
       const newVoteType = userVote === voteType ? null : voteType;
       const updatedVotes = await updateVotes(id, newVoteType);
-      setCurrentVotes((prevVotes) => {
-        if (userVote === 'up') {
-          return prevVotes - 1;
-        } else if (userVote === 'down') {
-          return prevVotes + 1;
-        } else if (newVoteType === 'up') {
-          return prevVotes + 1;
-        } else if (newVoteType === 'down') {
-          return prevVotes - 1;
-        }
-        return updatedVotes;
-      });
+      setCurrentVotes(updatedVotes); 
       setUserVote(newVoteType);
     } catch (error) {
+      console.error(error);
     }
   };
   
+
   return (
     <div>
       {loading ? (
@@ -61,24 +52,39 @@ function IndividualArticleCard() {
           <p>{article.body}</p>
           <p>Author: {article.author}</p>
           <p>Published: {article.created_at}</p>
-          <p>Votes: {currentVotes}</p> 
+          <p>Votes: {currentVotes}</p>
 
-          <button className={`vote-button ${userVote === 'up' ? 'upvoted' : ''}`} onClick={() => handleVote('up')}>
-            {userVote === 'up' ? '👍' : 'Upvote'}
-          </button>
-          <button className={`vote-button ${userVote === 'down' ? 'downvoted' : ''}`} onClick={() => handleVote('down')}>
-            {userVote === 'down' ? '👎' : 'Downvote'}
-          </button>
+          <div className="voting-buttons" style={{ width: '150px' }}> 
+            {userVote === 'up' ? (
+              <button className={`vote-button upvoted`} onClick={() => handleVote('up')}>
+                {'  Upvoted 👍'}
+              </button>
+            ) : userVote === 'down' ? null : (
+              <button className={`vote-button`} onClick={() => handleVote('up')}>
+                {'Upvote'}
+              </button>
+            )}
 
+            {userVote === 'down' ? (
+              <button className={`vote-button downvoted`} onClick={() => handleVote('down')}>
+                {' Downvoted 👎 '}
+              </button>
+            ) : userVote === 'up' ? null : (
+              <button className={`vote-button`} onClick={() => handleVote('down')}>
+                {'Downvote'}
+              </button>
+            )}
+          </div>
+          <div className="clearfix"></div>  {/* Added to clear layout after voting buttons */}
           <button className="toggle-button" onClick={toggleComments}>
             {showComments ? 'Hide Comments' : 'Show Comments'}
           </button>
-        
-          {showComments && ( 
+
+          {showComments && (
             <div className="comments">
               <h3>Comments</h3>
               <ul>
-                {articleComments.map(comment => (
+                {articleComments.map((comment) => (
                   <CommentCard key={comment.comment_id} comment={comment} />
                 ))}
               </ul>
